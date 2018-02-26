fa2c9ac33ad5470d90c6f745ec22201de448ec88
Bruno Pereira <brunno.pereira7@gmail.com>
2018-02-26 16:18:14 -0400

    função cria tabela

--- Server/Database/ConectaDB.php
+++ Server/Database/ConectaDB.php
@@ -36,10 +36,10 @@ Class ConectaDB{
     }
 
     public function fazSelect($sql,$array_obj){
-        $data = new ConectaDB();
-        $arrayReturn = $data->conectaDb();
 
-        if ($arrayReturn['result'] == true){
+        $arrayReturn = $this->conectaDb();
+
+        if ($arrayReturn['result']){
             $data = $arrayReturn['data'];
             $data->query($sql);
             if ($array_obj == 'obj') {
@@ -55,10 +55,10 @@ Class ConectaDB{
         }
     }
     public function execInsert($sql){
-        $data = new ConectaDB();
-        $arrayReturn = $data->conectaDb();
 
-        if ($arrayReturn['result'] == true){
+        $arrayReturn = $this->conectaDb();
+
+        if ($arrayReturn['result']){
             $data = $arrayReturn['data'];
             $query = $data->prepare($sql);
             try{
@@ -79,5 +79,38 @@ Class ConectaDB{
         }
     }
 
+    public function criaTabela($nomeTabela, $camposTabela){
+
+        $arrayReturn = $this->conectaDb();
+
+        if ($arrayReturn['result']){
+            $data = $arrayReturn['data'];
+            $sql = "CREATE TABLE IF NOT EXISTS $nomeTabela($camposTabela);";
+            $query = $data->prepare($sql);
+            try{
+                $data->beginTransaction();
+                $query->execute();
+                $data->commit();
+                $arrayReturn['data'] = '';
+                return $arrayReturn;
+
+            }catch (PDOExecption $e){
+                $data->rollBack();
+                $arrayReturn['result'] = false;
+                $arrayReturn['getMessage'] = $e->getMessage();
+                return $arrayReturn;
+            }
+        }else{
+            return $arrayReturn;
+        }
+
+    }
+
+    public function criaChaveEstrangeira($tabelaLocal, $constraint, $campoLocal, $campoReferencia, $tabelaReferencia){
+        $sql = "ALTER TABLE $tabelaLocal ADD CONSTRAINT $constraint FOREIGN KEY ( $campoLocal ) REFERENCES $campoReferencia ( $tabelaReferencia )";
+
+        // CONSTRAINT fk_id_empresa_cadastro FOREIGN KEY (id_empresa) REFERENCES tbl_empresa (id),
+        // CONSTRAINT fk_id_municipio_cadastro FOREIGN KEY (endereco_municipio_cod) REFERENCES tbl_municipio (codigo_municipio),
+    }
 
 }
